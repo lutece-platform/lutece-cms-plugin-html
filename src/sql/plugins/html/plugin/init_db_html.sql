c639fdfaa53bb4dd1e3f61d6bb6c94ac49be365a
pierrelevy <pierrelevy@users.noreply.github.com>
2018-08-25 01:22:24 +0200

    HTML-17 : SQL Scripts improvement

---
+++ src/sql/plugins/html/plugin/init_db_html.sql
@@ -0,0 +1,10 @@
+--
+-- Dumping data for table html_portlet
+--
+
+INSERT INTO html_portlet (id_portlet, html) VALUES 
+(1,'<div class=\"jumbotron\">\n  <h1>Welcome to LUTECE</h1>\n  <h3>\n    <small>Lutece is a CMS / Portal and a Web Framework that let\n    you create large and complex sites and applications.</small>\n  </h3>\n</div>\n'),
+(2,'<ul>\n  <li>Lutece is FreeSoftware. Full OpenSource licensed under\n  BSD.</li>\n  <li>Full responsive design Back and Front</li>\n  <li>Compliant with Bootstrap 3.x themes</li>\n  <li>Very modular and flexible architecture based on plugins,\n  APIs, IoC</li>\n  <li>Over 300 plugins available for many needs : Content\n  Management, Collaborative, Workflows, ...</li>\n  <li>Runs on Java Platform and rely on powerful build tools such\n  as Apache Maven</li>\n  <li>Uses best of breed Java Open Source stacks : Lucene, Spring,\n  Ehcache, Freemarker, ...</li>\n</ul>\n'),
+(3,'<form class=\"form\" action=\"jsp/admin/AdminLogin.jsp\" method=\"post\"> <div class=\"well\"> \n <span class=\"help-block\">Go and log in as \"admin\" or \"webmaster\".<br>The secret password is \"adminadmin\".</span> \n <br> \n <span class=\"help-block\">The \"admin\" user has all rights and features, whereas the \"webmaster\" user can only work on the editorial content of the site.</span> \n <br> \n <button class=\"btn btn-primary btn-block btn-flat\" type=\"submit\">Display login form</button> \n </div> </form>'),
+(4,'<h2>Here is a child page sample.</h2>\n<p>&#160;</p>\n<p>This page is divided into 2 columns.</p>\n<p>In each column you can publish several and different portlets :\nHTML, documents, ...</p>\n<p>This page can also have childs.</p>\n<p>&#160;</p>\n<p>&#160;</p>\n');
+
