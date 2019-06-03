97b02e732d729b759ea77e4dbf993278358513ba
Laurent HOHL <laurent.hohl@gmail.com>
2019-06-03 11:34:33 +0200

    HTML-17: Improve sample script

--- src/sql/plugins/html/core/init_core_html_data_sample.sql
+++ src/sql/plugins/html/core/init_core_html_data_sample.sql
@@ -3,10 +3,9 @@
 -- Dumping data for table core_portlet
 --
 
-INSERT INTO core_portlet (id_portlet, id_portlet_type, id_page, name, date_update, status, portlet_order, column_no, id_style, accept_alias, date_creation, display_portlet_title, role, device_display_flags) VALUES 
-(1,'HTML_PORTLET',1,'Qu\'est-ce que Lutece ?','2014-06-08 17:53:14',0,2,1,101,0,'2011-03-14 13:13:39',1,'none',273),
-(2,'HTML_PORTLET',1,'Software overview','2014-06-08 17:58:00',0,1,2,100,0,'2012-09-18 08:35:45',0,'none',273),
-(3,'HTML_PORTLET',1,'Back Office quick access','2014-06-08 17:49:37',0,1,3,100,0,'2009-05-15 04:38:08',0,'none',273),
-(4,'HTML_PORTLET',2,'Page 1','2014-06-08 18:28:50',0,1,1,100,0,'2014-06-08 18:27:59',0,'none',273);
+INSERT INTO `core_portlet` VALUES (1, 'HTML_PORTLET', 1, 'Qu\'est-ce que Lutece ?', '2019-05-31 16:28:04', 0, 2, 1, 100, 0, '2011-03-14 13:13:39', 1, 'none', 273);
+INSERT INTO `core_portlet` VALUES (2, 'HTML_PORTLET', 1, 'Infos', '2019-05-31 15:51:57', 0, 1, 3, 100, 0, '2015-05-15 15:26:01', 1, 'none', 273);
+INSERT INTO `core_portlet` VALUES (4, 'HTML_PORTLET', 1, 'Lutece', '2019-06-03 10:23:25', 0, 1, 4, 100, 0, '2019-05-31 11:55:16', 1, 'none', 4369);
+INSERT INTO `core_portlet` VALUES (5, 'HTML_PORTLET', 1, 'Autres informations', '2019-05-31 16:51:33', 0, 1, 5, 100, 0, '2019-05-31 15:01:47', 1, 'none', 4369);
 
 
