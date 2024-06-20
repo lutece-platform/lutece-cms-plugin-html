07b516a512ced59ee842670c9d0cbfb99d40d71e
chedli <chedly.hafaiedh@gmail.com>
2024-06-20 10:39:28 +0200

    LUT-28225:standardize sql scripts

--- src/sql/plugins/html/core/init_core_html_data_sample.sql
+++ src/sql/plugins/html/core/init_core_html_data_sample.sql
@@ -3,9 +3,7 @@
 -- Dumping data for table core_portlet
 --
 
-INSERT INTO `core_portlet` VALUES (1, 'HTML_PORTLET', 1, 'Qu\'est-ce que Lutece ?', '2019-05-31 16:28:04', 0, 2, 1, 100, 0, '2011-03-14 13:13:39', 1, 'none', 273);
-INSERT INTO `core_portlet` VALUES (2, 'HTML_PORTLET', 1, 'Infos', '2019-05-31 15:51:57', 0, 1, 3, 100, 0, '2015-05-15 15:26:01', 1, 'none', 273);
-INSERT INTO `core_portlet` VALUES (4, 'HTML_PORTLET', 1, 'Lutece', '2019-06-03 10:23:25', 0, 1, 4, 100, 0, '2019-05-31 11:55:16', 1, 'none', 4369);
-INSERT INTO `core_portlet` VALUES (5, 'HTML_PORTLET', 1, 'Autres informations', '2019-05-31 16:51:33', 0, 1, 5, 100, 0, '2019-05-31 15:01:47', 1, 'none', 4369);
-
-
+INSERT INTO core_portlet VALUES (1, 'HTML_PORTLET', 1, 'Qu''est-ce que Lutece ?', '2019-05-31 16:28:04', 0, 2, 1, 100, 0, '2011-03-14 13:13:39', 1, 'none', 273);
+INSERT INTO core_portlet VALUES (2, 'HTML_PORTLET', 1, 'Infos', '2019-05-31 15:51:57', 0, 1, 3, 100, 0, '2015-05-15 15:26:01', 1, 'none', 273);
+INSERT INTO core_portlet VALUES (4, 'HTML_PORTLET', 1, 'Lutece', '2019-06-03 10:23:25', 0, 1, 4, 100, 0, '2019-05-31 11:55:16', 1, 'none', 4369);
+INSERT INTO core_portlet VALUES (5, 'HTML_PORTLET', 1, 'Autres informations', '2019-05-31 16:51:33', 0, 1, 5, 100, 0, '2019-05-31 15:01:47', 1, 'none', 4369);
